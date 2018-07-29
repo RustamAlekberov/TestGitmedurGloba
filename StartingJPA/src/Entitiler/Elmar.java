5d64c50b166cb0e6d6fe8ed18ae3258f3b0279a2
RAlekberov77 <rustam.alekberoff@gmail.com>
2018-07-29 13:06:23 +0400

    elaci
    zordu

---
+++ StartingJPA/src/Entitiler/Elmar.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * and open the template in the editor.
+ */
+package Entitiler;
+
+/**
+ *
+ * @author rusta
+ */
+public class Elmar {
+    public static void main(String[] args) {
+        System.out.println("Salam dunya");
+    }
+}
